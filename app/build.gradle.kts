fb555fab62f1501af2873bfae8d916e8b90651ee
oscar mtzb <mtzb762@gmail.com>
2025-05-23 01:15:22 -0400

    [PREGUNTAR] cambiar la version en el build gradles

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.androidfactory.simplerick"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.androidfactory.simplerick"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(project(":network"))
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+
+    implementation(libs.navigation.compose)
+
+    // Image loading
+    implementation(libs.coil.compose)
+
+    // Dependency injection
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.compose.material:material-icons-core:1.5.4")
+
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.compose.ui.test.junit)
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
+}
+
+kapt {
+    correctErrorTypes = true
+}
---
+++ app/src/main/java/com/example/simplerick/components/character/CharacterDetailsNamePlateComponent.kt
@@ -0,0 +1,39 @@
+package com.androidfactory.simplerick.components.character
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.androidfactory.network.models.domain.CharacterStatus
+import com.androidfactory.simplerick.components.common.CharacterNameComponent
+import com.androidfactory.simplerick.ui.theme.RickAction
+
+@Composable
+fun CharacterDetailsNamePlateComponent(name: String, status: CharacterStatus) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        CharacterStatusComponent(characterStatus = status)
+        CharacterNameComponent(name = name)
+    }
+}
+
+@Preview
+@Composable
+fun NamePlatePreviewAlive() {
+    CharacterDetailsNamePlateComponent(name = "Rick Sanchez", status = CharacterStatus.Alive)
+}
+
+@Preview
+@Composable
+fun NamePlatePreviewDead() {
+    CharacterDetailsNamePlateComponent(name = "Rick Sanchez", status = CharacterStatus.Dead)
+}
+
+@Preview
+@Composable
+fun NamePlatePreviewUnknown() {
+    CharacterDetailsNamePlateComponent(name = "Rick Sanchez", status = CharacterStatus.Unknown)
+}
